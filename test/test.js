7937188ef188afd8f268a1f1ea05878b7f7f9690
wwweert123 <puachengxuan.fp@gmail.com>
2024-02-17 19:12:38 +0800

    Add test for subtract endpoint

--- test/test.js
+++ test/test.js
@@ -9,7 +9,7 @@ chai.use(chaiHttp);
 //Parent block
 describe("plus-minus", () => {
     /*
-     * Test the /GET route
+     * Test the /POST add route
      */
     describe("/POST add", () => {
         it("it should POST 2 numbers and return the added number", (done) => {
@@ -131,6 +131,142 @@ describe("plus-minus", () => {
 
             chai.request(server)
                 .post("/plus-minus/add")
+                .set("content-type", "application/x-www-form-urlencoded")
+                .send(params)
+                .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.should.be.a("object");
+                    res.body.should.have
+                        .property("message")
+                        .eql("number2 is not a number");
+                    done();
+                });
+        });
+    });
+
+    /*
+     * Test the /POST add route
+     */
+    describe("/POST subtract", () => {
+        it("it should POST 2 numbers and return the subtracted number", (done) => {
+            let params = {
+                number1: "1",
+                number2: "2",
+            };
+
+            chai.request(server)
+                .post("/plus-minus/subtract")
+                .set("content-type", "application/x-www-form-urlencoded")
+                .send(params)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a("object");
+                    res.body.should.have.property("answer").eql(-1);
+                    done();
+                });
+        });
+
+        it("it should POST 2 numbers and return the subtracted number, negative number used", (done) => {
+            let params = {
+                number1: "-100",
+                number2: "2",
+            };
+
+            chai.request(server)
+                .post("/plus-minus/subtract")
+                .set("content-type", "application/x-www-form-urlencoded")
+                .send(params)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a("object");
+                    res.body.should.have.property("answer").eql(-102);
+                    done();
+                });
+        });
+
+        it("it should POST 2 numbers and return the subtracted number, number1 missing", (done) => {
+            let params = {
+                number2: "2",
+            };
+
+            chai.request(server)
+                .post("/plus-minus/subtract")
+                .set("content-type", "application/x-www-form-urlencoded")
+                .send(params)
+                .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.should.be.a("object");
+                    res.body.should.have
+                        .property("message")
+                        .eql("Both numbers are required");
+                    done();
+                });
+        });
+
+        it("it should POST 2 numbers and return the subtracted number, number2 missing", (done) => {
+            let params = {
+                number1: "2",
+            };
+
+            chai.request(server)
+                .post("/plus-minus/subtract")
+                .set("content-type", "application/x-www-form-urlencoded")
+                .send(params)
+                .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.should.be.a("object");
+                    res.body.should.have
+                        .property("message")
+                        .eql("Both numbers are required");
+                    done();
+                });
+        });
+
+        it("it should POST 2 numbers and return the subtracted number, both number missing", (done) => {
+            let params = {};
+
+            chai.request(server)
+                .post("/plus-minus/subtract")
+                .set("content-type", "application/x-www-form-urlencoded")
+                .send(params)
+                .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.should.be.a("object");
+                    res.body.should.have
+                        .property("message")
+                        .eql("Both numbers are required");
+                    done();
+                });
+        });
+
+        it("it should POST 2 numbers and return the subtracted number, number1 not a number", (done) => {
+            let params = {
+                number1: "12adfasdf",
+                number2: "2",
+            };
+
+            chai.request(server)
+                .post("/plus-minus/subtract")
+                .set("content-type", "application/x-www-form-urlencoded")
+                .send(params)
+                .end((err, res) => {
+                    res.should.have.status(400);
+                    res.body.should.be.a("object");
+                    res.body.should.have
+                        .property("message")
+                        .eql("number1 is not a number");
+                    done();
+                });
+        });
+
+        it("it should POST 2 numbers and return the subtracted number, number2 not a number", (done) => {
+            let params = {
+                number1: "123123",
+                number2: "2adf32g",
+            };
+
+            chai.request(server)
+                .post("/plus-minus/subtract")
                 .set("content-type", "application/x-www-form-urlencoded")
                 .send(params)
                 .end((err, res) => {
