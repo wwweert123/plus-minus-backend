5cdec7aa42282b7778d518b5b54562cd6758e145
wwweert123 <puachengxuan.fp@gmail.com>
2024-02-17 15:18:10 +0800

    Install cors, set up server running on port 3500

--- server.js
+++ server.js
@@ -3,3 +3,12 @@ const app = express();
 const path = require("path");
 const cors = require("cors");
 const corsOptions = require("./config/corsOptions");
+const PORT = process.env.PORT || 3500;
+
+// Options for CORS
+app.use(cors(corsOptions));
+
+// built-in middleware for json
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
